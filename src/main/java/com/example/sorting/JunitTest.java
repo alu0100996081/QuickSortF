8f301803408a28df8165686a24ef2374075f8518
alu0100996081 <alu0100996081@ull.edu.es>
2018-12-16 18:34:23 +0000

    Añadidos comentarios en doxygen
    comantadas las clase quicksort y de prueba

--- src/main/java/com/example/sorting/JunitTest.java
+++ src/main/java/com/example/sorting/JunitTest.java
@@ -12,12 +12,16 @@ class JunitTest {
 		int [] input = new int[]{6,1,7,9,3,8,2,5,4,0};
 		Quicksort sort = new Quicksort();
 		sort.sort(input);
-		
 		String result = new String();
 	
 		for(int i:input)
 			result = result + " " + i;
 		
+		 /**
+         * Comprobamos que la lista desordenada que le pasamos da como salida 
+         * la lista ordenada, cumpliendo el formato que se le pasa como string. 
+         * 
+         */ 
 		assertEquals(" 0 1 2 3 4 5 6 7 8 9", result);
 
 	}
--- src/main/java/com/example/sorting/Quicksort.java
+++ src/main/java/com/example/sorting/Quicksort.java
@@ -1,12 +1,17 @@
 package com.example.sorting;
-
+/**
+ * Quicksort JunitTest Pom
+ * @author Daniel Fumero Cruz
+ */ 
 public class Quicksort  {
 
 	private int[] numbers;
 	private int number;
 
 	public void sort(int[] values) {
-		// Check for empty or null array
+		/**
+		  * Check for empty or null array
+		 */ 
 		if (values == null || values.length == 0){
 			return;
 		}
@@ -21,39 +26,46 @@ public class Quicksort  {
 	private void quicksort(int low, int high) {
 
 		int i = low, j = high;
-
-		// Get the pivot element from the middle of the list
+		/**
+		  * Get the pivot element from the middle of the list
+		 */ 
 		int pivot = numbers[low + (high-low) / 2];
-
-		// Divide into two lists
+		/**
+		  * Divide into two lists
+		 */
 		while (i <= j) {
-
-			// If the current value from the left list is smaller then the pivot
-			// element then get the next element from the left list
+			/**
+			  * If the current value from the left list is smaller then the pivot
+			  * element then get the next element from the left list
+			*/
 			while (numbers[i] < pivot) {
 				i++;
 			}
-
-			// If the current value from the right list is larger then the pivot
-			// element then get the next element from the right list
+			/**
+			* If the current value from the right list is larger then the pivot
+			* element then get the next element from the right list
+			*/
 			while (numbers[j] > pivot) {
 				j--;
 			}
-
-			// If we have found a values in the left list which is larger then
-			// the pivot element and if we have found a value in the right list
-			// which is smaller then the pivot element then we exchange the
-			// values.
-
-			// As we are done we can increase i and j
+			/**
+			* If we have found a values in the left list which is larger then
+			* the pivot element and if we have found a value in the right list
+			* which is smaller then the pivot element then we exchange the
+			* values.
+			*/
+			/**
+			* As we are done we can increase i and j
+			*/
 			if (i <= j) {
 				exchange(i, j);
 				i++;
 				j--;
 			}
 		}
-
-		// Recursion
+		/**
+		* Recursion
+		*/
 		if (low < j)
 			quicksort(low, j);
 
@@ -73,8 +85,21 @@ public class Quicksort  {
 	 public static void main(String a[]){
          
 	        Quicksort sorter = new Quicksort();
+
+	        /**
+	         * Creamos un vector de enteros desordenado. 
+	         * 
+	         */ 
 	        int[] input = {24,2,45,20,56,75,2,56,99,53,12};
+	        /**
+	         * Le pasamos el vector a la funcion sort para que lo ordene. 
+	         * 
+	         */ 
 	        sorter.sort(input);
+	        /**
+	         * Mostramos los elementos del vector ya ordenados. 
+	         * 
+	         */ 
 	        for(int i:input){
 	            System.out.print(i);
 	            System.out.print(" ");
